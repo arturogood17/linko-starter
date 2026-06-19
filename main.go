a0b90b3e1e668ce23023ae01fde6873d24c6ffd5
arturogood17 <arturo_good17@hotmail.com>
2026-06-19 13:07:35 +0200

    Now slog is being used for logging instead of normal log to be able to use structered logging

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -37,15 +37,17 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		return 1
 	}
 	defer func() error {
-		if err := closingFunc(); err != nil {
-			fmt.Fprintf(os.Stderr, "Error al intentar cerrar el archivo: %v", err)
+		if closingFunc != nil {
+			if err := closingFunc(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error al intentar cerrar el archivo: %v", err)
+			}
 		}
 		return nil
 	}()
 
 	st, err := store.New(dataDir, logger)
 	if err != nil {
-		logger.Printf("failed to create store: %v", err)
+		logger.Info(fmt.Sprintf("failed to create store: %v", err))
 		return 1
 	}
 	s := newServer(*st, httpPort, cancel, logger)
@@ -59,17 +61,17 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		logger.Printf("failed to shutdown server: %v", err)
+		logger.Info(fmt.Sprintf("failed to shutdown server: %v", err))
 		return 1
 	}
 	if serverErr != nil {
-		logger.Printf("server error: %v", serverErr)
+		logger.Info(fmt.Sprintf("server error: %v", serverErr))
 		return 1
 	}
 	return 0
 }
 
-func initializeLogger() (*log.Logger, closeFunction, error) {
+func initializeLogger() (*slog.Logger, closeFunction, error) {
 	if os.Getenv(("LINKO_LOG_FILE")) != "" {
 		file, err := os.OpenFile("linko.access.log", os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0o644)
 		if err != nil {
@@ -77,7 +79,7 @@ func initializeLogger() (*log.Logger, closeFunction, error) {
 		}
 		bufferFile := bufio.NewWriterSize(file, 8192)
 		multilogger := io.MultiWriter(os.Stderr, bufferFile)
-		return log.New(multilogger, "", log.LstdFlags), func() error {
+		return slog.New(slog.NewTextHandler(multilogger, nil)), func() error {
 			if err := bufferFile.Flush(); err != nil {
 				return err
 			}
@@ -87,5 +89,5 @@ func initializeLogger() (*log.Logger, closeFunction, error) {
 			return nil
 		}, nil
 	}
-	return log.New(os.Stderr, "", log.LstdFlags), nil, nil
+	return slog.New(slog.NewTextHandler(os.Stderr, nil)), nil, nil
 }
